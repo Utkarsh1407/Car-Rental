4acb10f13931fccee1d0928ce135a4e6e8ba9fad
Suhani Garg <suhanigarg@Suhanis-MacBook-Air.local>
2025-12-28 19:59:29 +0530

    Imagekit added

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,4 +1,7 @@
+import imagekit from "../configs/imagekit.js"
 import User from "../models/user.js"
+import fs from "fs"
+import Car from "../models/car.js"
 
 
 export const changeRoleToOwner = async(req,res) => {
@@ -12,5 +15,35 @@ export const changeRoleToOwner = async(req,res) => {
 }
 
 export const addCar = async(req,res) => {
-    
+    try{
+        const {_id} = req.user
+        let car = JSON.parse(req.body.carData)
+        const imageFile = req.file
+
+        // upload images to imagekit
+
+        const fileBuffer = fs.readFileSync(imageFile.path)
+        const response = await imagekit.upload({
+            file:fileBuffer,
+            fileName: imageFile.originalname,
+            folder: "/cars"
+        })
+
+        var optimizedimageUrl = imagekit.url({
+            path: response.filePath,
+            transformation: [
+                {width : "1280"},
+                {height : "auto"},
+                {format : "webp"}
+            ]
+        })
+        
+        const image = optimizedimageUrl;
+        await Car.create({...car, owner:_id, image})
+
+        res.json({success:true, message:"Car is added"})
+
+    }catch(error){
+        res.json({success:false,message:error.message})
+    }
 }
